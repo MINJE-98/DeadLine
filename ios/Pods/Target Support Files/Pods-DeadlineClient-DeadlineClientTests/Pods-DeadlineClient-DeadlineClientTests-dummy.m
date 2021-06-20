bcd4a60aa22617411f1e78fd2c57780484b34db9
MINJE-98 <jmj012100@gmail.com>
2021-06-20 17:05:08 +0900

    back

---
+++ ios/Pods/Target Support Files/Pods-DeadlineClient-DeadlineClientTests/Pods-DeadlineClient-DeadlineClientTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DeadlineClient_DeadlineClientTests : NSObject
+@end
+@implementation PodsDummy_Pods_DeadlineClient_DeadlineClientTests
+@end
